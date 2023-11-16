527a9cdef48d5612e78d0d251b32f70ea9d6b37e
Adrian Esau <adrian.esau@student.ufv.ca>
2023-11-16 15:26:50 -0800

    added Scrollup button

--- Script/script.js
+++ Script/script.js
@@ -1,10 +1,19 @@
-// Scrolls to 100 px above the referenced section so that the title of the referenced section
+// Scrolls to 60 px above the referenced section so that the title of the referenced section
 // ends up in the current view even if there is an overlapping element at the top
 function scrolltoSection(sectionId) {
 	let element = document.getElementById(sectionId);
 	let offset = 60;
 	window.scrollTo({
 		top: element.offsetTop - offset,
+		behavior: 'smooth'
+	});
+}
+
+// Scrolls to 100 px above the referenced section so that the title of the referenced section
+// ends up in the current view even if there is an overlapping element at the top
+function scrollUp() {
+	window.scrollTo({
+		top: 0,
 		behavior: 'smooth'
 	});
 }
