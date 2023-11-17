c4b98aa4b35d786805e31e30637673a5e214b037
Adrian Esau <adrian.esau@student.ufv.ca>
2023-11-16 23:00:51 -0800

    initial-commit

---
+++ hosting/src/main.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyAZV5sajK5IOVU_EEksXyN0gpcGoJlsuE8",
+  authDomain: "hammrpg-c7378.firebaseapp.com",
+  projectId: "hammrpg-c7378",
+  storageBucket: "hammrpg-c7378.appspot.com",
+  messagingSenderId: "385634865690",
+  appId: "1:385634865690:web:0306042c5bae974b4737a3",
+  measurementId: "G-LTK3FFYZ11"
+})
+
+const auth = getAuth(firebaseApp);
+const button = document.querySelector('button');
+
+onAuthStateChanged(auth, user => {
+  if (user == null) { return; }
+  console.log(user);
+})
+
+button?.addEventListener('click', clickEvent => {
+  signInWithRedirect(auth, new GoogleAuthProvider())
+});
