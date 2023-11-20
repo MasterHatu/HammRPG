c78853bfac2c7e2e49494778fa1c3acfeff31783
Adrian Esau <adrian.esau@student.ufv.ca>
2023-11-20 02:49:19 -0800

    edit

--- script/script.js
+++ script/script.js
@@ -1,23 +1,17 @@
 // Scrolls to 60 px above the referenced section so that the title of the referenced section
 // ends up in the current view even if there is an overlapping element at the top
-function scrolltoSection(sectionId: string): void {
-	const element = document.getElementById(sectionId);
-  
-	if (element) {
-		const offset = 60;
-
-		window.scrollTo({
+function scrolltoSection(sectionId) {
+	let element = document.getElementById(sectionId);
+	let offset = 60;
+	window.scrollTo({
 		top: element.offsetTop - offset,
 		behavior: 'smooth'
-		});
-	} else {
-		console.error(`Element with ID ${sectionId} not found.`);
-	}
+	});
 }
 
 // Scrolls to 100 px above the referenced section so that the title of the referenced section
 // ends up in the current view even if there is an overlapping element at the top
-function scrollUp(): void {
+function scrollUp() {
 	window.scrollTo({
 		top: 0,
 		behavior: 'smooth'
